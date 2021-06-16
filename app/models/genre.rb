74a3b5ad9c3e0c190cf911b5a997b58406bd79b6
Ishii-Kaho <kh141smallbear@gmail.com>
2021-06-16 12:51:18 +0000

    SCSS-fix

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ApplicationRecord
   has_many :posts
-  
+
   enum genre: {
     "--ジャンル選択--": 0, グルメ: 1, トイレ: 2, 喫煙所: 3
   }
--- app/models/post.rb
+++ app/models/post.rb
@@ -45,6 +45,4 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :opinion, presence: true
   validates :visit_time, presence: true
-
-
 end
